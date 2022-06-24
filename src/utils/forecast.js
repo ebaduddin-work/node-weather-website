77a58d8cb34ae27261a30bb872b0545a93417528
Ebaduddin Mohammed <mdebad@hotmail.com>
2022-06-24 04:37:10 -0400

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback)  => {
         }else if(body.length === 63){
             callback('Unable to find location', undefined)
         }else{
-            callback(undefined, body.days[0].description + ' It is currently ' + body.currentConditions.temp + ' degrees out. There is a ' + body.days[0].precipprob + '% chance of rain.')
+            callback(undefined, body.days[0].description + ' It is currently ' + body.currentConditions.temp + ' degrees out. The high today is ' + body.days[0].tempmax + ' with a low of ' + body.days[0].tempmin + '. There is a ' + body.days[0].precipprob + '% chance of rain. ')
 
         }
     })
